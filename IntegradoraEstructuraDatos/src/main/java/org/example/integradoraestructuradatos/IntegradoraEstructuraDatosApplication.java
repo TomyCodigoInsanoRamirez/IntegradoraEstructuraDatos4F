ae96466c7de245f1d2fda4263dbe0946ae8388e4
TomyCodigoInsanoRamirez <168792814+TomyCodigoInsanoRamirez@users.noreply.github.com>
2024-11-30 11:30:31 -0600

    Add files via upload

---
+++ IntegradoraEstructuraDatos/src/main/java/org/example/integradoraestructuradatos/IntegradoraEstructuraDatosApplication.java
@@ -0,0 +1,13 @@
+package org.example.integradoraestructuradatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradoraEstructuraDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradoraEstructuraDatosApplication.class, args);
+    }
+
+}
---
+++ IntegradoraEstructuraDatos/src/test/java/org/example/integradoraestructuradatos/IntegradoraEstructuraDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.integradoraestructuradatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradoraEstructuraDatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
