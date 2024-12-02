5617ebaea4c50bf0d476f43e73ef86e6a810897a
RichartM <https://github.com/RichartM/PruebaN.git>
2024-12-02 15:49:13 -0600

    ClienteController

---
+++ IntegradoraEstructuraDatos/src/main/java/org/example/integradoraestructuradatos/controller/ClienteController.java
@@ -0,0 +1,20 @@
+package org.example.integradoraestructuradatos.controller;
+import org.example.integradoraestructuradatos.model.Cliente;
+import org.example.integradoraestructuradatos.dao.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("/agregarCliente")
+    public ResponseEntity<Cliente> agregarCliente(@RequestBody Cliente cliente) {
+        Cliente nuevoCliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(nuevoCliente);
+    }
+}
